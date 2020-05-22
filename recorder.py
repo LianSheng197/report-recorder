aa019e4f614d96942ef472c6daff915411f27321
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-05-22 13:52:06 +0800

    基本架構

--- recorder.py
+++ recorder.py
@@ -6,14 +6,30 @@ import json
 import time
 import sys
 import signal
+import requests
+from urllib import parse
+
 
 def fetchData():
-    print()
+    c.execute("SELECT * FROM reports ORDER BY id DESC")
+
+    lastRow = c.fetchone()
+
+    if(lastRow != None):
+        print(lastRow[-1])
+    else:
+        print(0)
+
+    # data = requests.get(url=reportUrl, headers=headers).json()
+    # for report in data["reports"]:
+    #     print(report)
+
 
 def handler(sig, frame):
     print('已停止')
     conn.close()
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, handler)
 
@@ -43,6 +59,18 @@ if(not isinstance(_config["report"]["warningLevel"], int) or _config["report"]["
     print("設定錯誤： report.warningLevel 必須是 1, 2, 3 其一")
     sys.exit(205)
 
+
+battleType = {'0': '友好切磋',
+              '1': '認真決鬥',
+              '2': '決一死戰',
+              '3': '我要殺死你'}
+reportUrl = "https://mykirito.com/api/reports"
+headers = {
+    'content-type': 'application/json;charset=UTF-8',
+    'token': _config["token"],
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'}
+
+
 conn = sqlite3.connect('reports.sqlite3')
 print("成功連接資料庫")
 
@@ -53,15 +81,44 @@ c.execute(
 )
 if c.fetchone()[0] == 1:
     print('資料表已存在，開始記錄')
+
+    while True:
+        fetchData()
+        time.sleep(_config["report"]["period"])
 else:
     print('資料表不存在，先建表')
 
+    # id            資料 ID，主鍵
+    # playerName    玩家暱稱
+    # playerUid     玩家 ID
+    # playerLevel   玩家等級
+    # myLevel       我方等級
+    # type          戰鬥種類（友好切磋, 認真決鬥, 決一死戰, 我要殺死你）
+    # result        戰鬥結果（勝利, 戰敗）
+    # reportId      戰報 ID
+    # hasShout      是否留言（true, false）
+    # timestamp     戰報時間（秒）
+    conn.execute(
+        '''
+        CREATE TABLE reports
+        (
+            id INTEGER PRIMARY KEY  AUTOINCREMENT,
+            playerName     TEXT     NOT NULL,
+            playerUid      CHAR(30) NOT NULL,
+            playerLevel    INT      NOT NULL,
+            myLevel        INT      NOT NULL,
+            type           TEXT     NOT NULL,
+            result         TEXT     NOT NULL,
+            reportId       CHAR(30) NOT NULL,
+            hasShout       INT      NOT NULL,
+            timestamp      INT      NOT NULL
+        );
+        '''
+    )
+
+    while True:
+        fetchData()
+        time.sleep(_config["report"]["period"])
+
 conn.commit()
 conn.close()
-
-while True:
-    print("loop")
-    time.sleep(1)
-
-
-
